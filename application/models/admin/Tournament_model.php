0e9e25b980ec20d5ba50bfdbdefd82c72ee25c75
Jacob Hansen <azarat@mail.tele.dk>
2013-01-10 21:53:09 +0000

    Added commenting to the Tournament_model Fixed an issue that caused a tournament ending with the searched date not to be found.

--- application/models/admin/Tournament_model.php
+++ application/models/admin/Tournament_model.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Model for Umpire administration
+ *
+ * Created: 01/11/2013
+ * @author	Jacob Baungard Hansen <jeb14@hw.ac.uk>
+ */
 class Tournament_model extends CI_Model {
 
 	public function __construct()
@@ -15,25 +21,44 @@ class Tournament_model extends CI_Model {
 		return $this->db->insert_id();
 	}
 	
+	/**
+	 * Convenience function for getting a count of all future tournaments.
+	 *
+	 * @return 			count of all future tournaments
+	 */
 	public function tournamentCountFuture()
 	{
 		$date = new DateTime();
 		return $this->tournamentCount($date->format("Y-m-d"));
 	}
 	
+	/**
+	 * Convenience function for getting a count of all past tournaments.
+	 *
+	 * @return 			count of all past tournaments
+	 */
 	public function tournamentCountPast()
 	{
 		$date = new DateTime();
 		return $this->tournamentCount($date->format("Y-m-d"), true);
 	}
-	
-	public function tournamentCount($date = false, $past = false)
+	/**
+	 * Dunction for getting the count of tournaments (past and/or future not all)
+	 * If date is NOT set it will get count of all tournaments
+	 * If date is set and past is false (or not set) it will get all tournaments after the set date
+	 * if past is set to true it will find all tournaments before the specified date
+	 *
+	 * @param date		The date to filter by
+	 * @param past		If true filter by tournaments before the set date
+	 * @return 			A count of tournaments
+	 */
+	private function tournamentCount($date = false, $past = false)
 	{
 		if ($date != false)
 		{
 			if ($past == false)
 			{
-				$logic_operator = ">";
+				$logic_operator = ">=";
 			}
 			else
 			{
@@ -44,22 +69,50 @@ class Tournament_model extends CI_Model {
 		return $this->db->count_all_results("tournaments");
 	}
 
+	/**
+	 * Convenience function for getting a tournament with a specific ID.
+	 *
+	 * @param id		id of the tournament
+	 * @return 			the fetched tournament
+	 */
 	public function getTournamentId($id)
 	{
 		return $this->getTournament($id, $limit = false, $start = false);
 	}
+	
+	/**
+	 * Convenience function for getting a list of tournaments (past and future) with pagination
+	 *
+	 * @param limit		how many records should be fetched
+	 * @param start		page off-set.
+	 * @return 			A list of future tournaments
+	 */
 	
 	public function getTournamentListLimit($limit, $start)
 	{
 		return $this->getTournament(false, $limit, $start);
 	}
 	
+	/**
+	 * Convenience function for getting a list of future tournaments with pagination
+	 *
+	 * @param limit		how many records should be fetched
+	 * @param start		page off-set.
+	 * @return 			A list of future tournaments
+	 */
 	public function getFutureTournaments($limit, $start)
 	{
 		$date = new DateTime();
 		return $this->getTournament(false, $limit, $start, $date->format("Y-m-d"));
 	}
 	
+	/**
+	 * Convenience function for getting a list of all past tournaments with pagination
+	 *
+	 * @param limit		how many records should be fetched
+	 * @param start		page off-set.
+	 * @return 			A list of finished tournaments
+	 */
 	public function getPastTournament($limit, $start)
 	{
 		$date = new DateTime();
@@ -84,7 +137,7 @@ class Tournament_model extends CI_Model {
 			{
 				if ($past == false)
 				{
-					$logic_operator = ">";
+					$logic_operator = ">=";
 				}
 				else
 				{
@@ -109,10 +162,7 @@ class Tournament_model extends CI_Model {
 		return $query->row_array();
 	}
 	
-	/**
-  	 * Updates a given tournament.
-	 * @param row	the row to be updated
-     */
+ 
 	public function update($row)
 	{
 		$this->db->where('tournamentId', $row['tournamentId']);
