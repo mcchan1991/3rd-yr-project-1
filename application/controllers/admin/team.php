0304a4545ed23cdfbcb2beeb080289446ee3fee1
Doix <johnnydval@gmail.com>
2013-03-17 17:36:07 +0000

    updated admin team registration

--- application/controllers/admin/team.php
+++ application/controllers/admin/team.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class team extends My_Admin_Controller {
-	function __construct()
+function __construct()
 	{
 		parent::__construct();
 		
@@ -16,17 +16,33 @@ class team extends My_Admin_Controller {
 		$this->register($eventId);
 	}
 	
-	function register($eventId)
+	function register($eventId,$prefill = NULL)
 	{
 		$this->load->helper('form');
 		$event = $this->Event_model->getEvent($eventId);
 		//$event = $this->Event_model->getId($eventId);
 		// need to set these as null to make sure no warnings come up (prepolation the form if validation error or edit)
-		$data['nwaId'] = "";
-		$data['name'] = "";
-		$data['contactFirstName'] = "";
-		$data['contactSurname'] = "";
-		$data['email'] = "";
+		if ($prefill != NULL)
+		{
+			$data['nwaId'] = $prefill['nwaId'];
+			$data['name'] =  $prefill['name'];
+			$data['contactFirstName'] =  $prefill['contactFirstName'];
+			$data['contactSurname'] =  $prefill['contactSurname'];
+			$data['firstName'] =  $prefill['firstName'];
+			$data['surname'] =  $prefill['surname'];
+			$data['num'] =  $prefill['num'];
+			$data['email'] = $prefill['email'];
+			$data['description'] =  $prefill['description'];
+		}
+		else
+		{
+			$data['nwaId'] = "";
+			$data['name'] =  "";
+			$data['contactFirstName'] =  "";
+			$data['contactSurname'] =  "";
+			$data['email'] =  "";
+			$data['description'] =  "";
+		}
 		$data['password'] = "";
 		$data['cpassword'] = "";
 		$data['eventId'] = "";
@@ -46,6 +62,8 @@ class team extends My_Admin_Controller {
 		$regStart = DateTime::createFromFormat($dateFormat, $event['regStart']);
 		$regEnd = DateTime::createFromFormat($dateFormat, $event['regEnd']);
 		
+		$data['registrationError'] = 0; // no error by default
+		
 		if ($currentDate > $regEnd)
 		{
 			$data['registrationError'] = 1;
@@ -54,34 +72,74 @@ class team extends My_Admin_Controller {
 		{
 			$data['registrationError'] = 2;
 		}
-		else if ($this->Event_model->getEventRegistrationsCount($eventId) == $event['maxEntries'])
+		else if ($this->Event_model->getEventRegistrationsCount($eventId) >= $event['maxEntries'])
 		{
 			$data['registrationError'] = 3;
 		}
-		
 		$this->template->write_view('content','admin/team/NewTeam',$data);
 		$this->template->render();
 	}
 	
 	public function add($eventId)
 	{
+		
 		$this->load->library('form_validation');
 		$this->load->model('team/Team_model');
 		
 		$this->form_validation->set_rules('nwaId', 'nwaId', 'required|trim|callback_checkUniqueNWAID');
 		$this->form_validation->set_rules('name', 'name', 'required|trim|callback_checkUniqueTeamName|min_length[1]|max_length[50]');
 		$this->form_validation->set_rules('email', 'email', 'required|trim|valid_email|callback_checkUniqueEmail');
-		$this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[8]|max_length[50]');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]|max_length[50]');
 		$this->form_validation->set_rules('cpassword', 'cpassword', 'required|trim|matches[password]');
 		$this->form_validation->set_rules('contactFirstName', 'Contact First Name', 'required|trim');
 		$this->form_validation->set_rules('contactSurname', 'Contact Surname', 'required|trim');
+		$this->form_validation->set_rules('firstName[]', 'First Name', 'required|trim');
+		$this->form_validation->set_rules('surname[]', 'Surname', 'required|trim');
+		$this->form_validation->set_rules('num[]', 'Shirt Number', 'required|trim');
+		
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '1000';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+		$config['overwrite'] = TRUE;
+
+		if (isset ( $_FILES['userfile']['name']))
+		{
+			$name = $_FILES['userfile']['name']; // get file name from form
+			$fileNameParts   = explode( '.', $name ); // explode file name to two part
+			$fileExtension   = end( $fileNameParts ); // give extension
+			$fileExtension   = strtolower( $fileExtension ); // convert to lower case
+			$encripted_pic_name   = md5($this->input->post('nwaId')).'.'.$fileExtension;  // new file name
+			$config['file_name'] = $encripted_pic_name; //set file name
+		}
+		
+		$this->load->library('upload', $config);
 		
 		if($this->form_validation->run()==false)
 		{
-			$this->register($eventId);
+			$prefill = array(
+			'nwaId' => $this->input->post('nwaId'),
+			'name' => $this->input->post('name'),
+			'contactFirstName' => $this->input->post('contactFirstName'),
+			'contactSurname' => $this->input->post('contactSurname'),
+			'email' => $this->input->post('email'),
+			'firstName' => $this->input->post('firstName'),
+			'surname' => $this->input->post('surname'),
+			'num' => $this->input->post('num'),
+			'description' => $this->input->post('description'),
+			);
+			$this->register($eventId,$prefill);
 		}
 		else
 		{
+			$this->upload->do_upload("userfile");
+			$data = array('upload_data' => $this->upload->data());
+			if ($this->upload->display_errors() != "You did not select a file to upload." && $this->upload->display_errors() != NULL)
+			{
+				$this->register($eventId);
+			}
+			$upload_data = $this->upload->data();
 			$data = array(
 			'nwaId' => $this->input->post('nwaId'),
 			'name' => $this->input->post('name'),
@@ -89,6 +147,8 @@ class team extends My_Admin_Controller {
 			'contactSurname' => $this->input->post('contactSurname'),
 			'email' => $this->input->post('email'),
 			'password' => sha1($this->input->post('password')),
+			'description' => $this->input->post('description'),
+			'logo' => $upload_data['is_image'] != NULL ? 1 : 0
 			);
 			$this->Team_model->create($data);
 			$data1 = array(
@@ -99,7 +159,21 @@ class team extends My_Admin_Controller {
 			);
 			$this->Team_model->createTeamReg($data1);
 			
-			redirect('admin/', 'refresh');
+			$firstName = $this->input->post('firstName');
+			$surname = $this->input->post('surname');
+			$num = $this->input->post('num');
+			
+			for ($i = 0; $i < 10; $i++)
+			{
+				$data = array (
+				'nwaId' => $this->input->post('nwaId'),
+				'shirtNo' => $num[$i],
+				'firstName' => $firstName[$i],
+				'surname' => $surname[$i]
+				);
+				$this->Team_model->createPlayer($data);
+			}
+			redirect('', 'refresh');
 		}
 	}
 	
--- application/views/admin/team/NewTeam.php
+++ application/views/admin/team/NewTeam.php
@@ -18,167 +18,197 @@ $btnAttributes = array(
 </ul>
 <h3>Register for event: <?php echo $event['name'] ?></h3>
 <?php
-$errors = validation_errors();
-if (!empty($errors) || isset($registrationError))
-{
-echo "<div class=\"alert alert-error\">";
-echo "Sorry, registration for this tournament have ended.";
-
-echo "</div>";
-}
-else if (isset($registrationError))
-{
-echo "<div class=\"alert alert-error\">";
-if ($registartionError == 1)
-{
-	echo "Sorry, registration for this tournament have ended.";
-}
-else if ($registartionError == 2)
-{
-	echo "Sorry, registration for this tournament have not yet startet. Registration period is: PLACEHOLDER";
-}
-else if ($registrationError == 3)
-{
-	echo "Sorry, this event have reached its max entries, and no more registrations are allowed";
-}
-echo "</div>";
-
-$btnAttributes = array(
-    'class' => 'btn disabled',
-);
-}
-
-?>
-<?php echo validation_errors(); ?>
-<?php
-	echo form_open("admin/team/add/{$event['eventId']}", $attributes);
-	//echo form_open('team/teamRegister/add');
-
-	// form building
-	$nwaId = array(
-		'name'	=> 'nwaId',
-		'id'	=> 'nwaId',
-		'value' => $nwaId
-	);
-
-	echo "<div class=\"control-group\">";
-	echo form_label('NWAID', 'nwaId', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($nwaId);
-	echo '</div></div>';
-	
-
-	
-	$name = array(
-		'name'	=> 'name',
-		'id'	=> 'name',
-		'value' => $name
-	);
-
-	echo "<div class=\"control-group\">";
-	echo form_label('Team Name', 'name', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($name);
-	echo '</div></div>';
-		
-
-	
-	$contactFirstName = array(
-		'name'	=> 'contactFirstName',
-		'id'	=> 'contactFirstName',
-		'value' => $contactFirstName
-	);
-	
-	echo "<div class=\"control-group\">";
-	echo form_label('Contact First Name', 'contactFirstName', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($contactFirstName);
-	echo '</div></div>';
-	
-
-	
-
-	
-	
-
-	$contactSurname = array(
-		'name'	=> 'contactSurname',
-		'id'	=> 'contactSurname',
-		'value' => $contactSurname
-	);
-	
-	
-	echo "<div class=\"control-group\">";
-	echo form_label('Contact Surname', 'contactSurname', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($contactSurname);
-	echo '</div></div>';
-	
-	$password = array(
-		'name'	=> 'password',
-		'id'	=> 'password',
-		'value' => $password
-	);
-	
-	echo "<div class=\"control-group\">";
-	echo form_label('Password', 'password', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_password($password);
-	echo '</div></div>';
-
-	$cpassword = array(
-		'name'	=> 'cpassword',
-		'id'	=> 'cpassword',
-		'value' => $cpassword
-	);
-	
-	
-	echo "<div class=\"control-group\">";
-	echo form_label('Confirm Password', 'cpassword', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_password($cpassword);
-	echo '</div></div>';
-	
-	$email = array(
-		'name'	=> 'email',
-		'id'	=> 'email',
-		'value' => $email
-	);
-	
-	
-	echo "<div class=\"control-group\">";
-	echo form_label('E-mail', 'email', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($email);
-	echo '</div></div>';
-	
-	$id = array(
-		'name'	=> 'id',
-		'id'	=> 'id',
-		'value' => $event['eventId']
-	);
-	
-	
-	/*echo "<div class=\"control-group\">";
-	echo form_label('eventId', 'id', $labelAttributes);
-	echo "<div class=\"controls\">";
-	echo form_input($id);
-	echo '</div></div>';*/
-	
-	echo "<div class=\"control-group\">";
-	echo "<div class=\"controls\">";
-	
-	// codeigniter was causing some troubles so submit button in pure php
-	if (!empty($errors) || isset($registrationError))
+	$errors = validation_errors();
+	if (!empty($errors))
 	{
-		echo "<input type=\"submit\" name=\"\" value=\"Submit\" class=\"btn disabled\" disabled />";
+		echo "<div class=\"alert alert-error\">";
+		echo validation_errors();
+		echo "</div>";
+	}
+	
+	if ($registrationError != 0)
+	{
+		switch ($registrationError) {
+			case 1:
+				echo "Registration for this event is closed"; break;
+			case 2:
+				echo "Registration for this event hasn't started yet"; break;
+			case 3:
+				echo "This event is already full"; break;
+		}
 	}
 	else
 	{
-		echo "<input type=\"submit\" name=\"\" value=\"Submit\" class=\"btn\" submit />";
-		
-	}
-	echo '</div>';
-	echo '</div>';
 	
-	echo form_close();
+		echo form_open_multipart("team/teamRegister/add/{$event['eventId']}", $attributes);
+		//echo form_open('team/teamRegister/add');
+
+		// form building
+		$nwaId = array(
+			'name'	=> 'nwaId',
+			'id'	=> 'nwaId',
+			'value' => $nwaId
+		);
+
+		echo "<div class=\"control-group\">";
+		echo form_label('NWAID', 'nwaId', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_input($nwaId);
+		echo '</div></div>';
+		
+
+		
+		$name = array(
+			'name'	=> 'name',
+			'id'	=> 'name',
+			'value' => $name
+		);
+
+		echo "<div class=\"control-group\">";
+		echo form_label('Team Name', 'name', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_input($name);
+		echo '</div></div>';
+			
+
+		
+		$contactFirstName = array(
+			'name'	=> 'contactFirstName',
+			'id'	=> 'contactFirstName',
+			'value' => $contactFirstName
+		);
+		
+		echo "<div class=\"control-group\">";
+		echo form_label('Contact First Name', 'contactFirstName', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_input($contactFirstName);
+		echo '</div></div>';
+		
+
+		$contactSurname = array(
+			'name'	=> 'contactSurname',
+			'id'	=> 'contactSurname',
+			'value' => $contactSurname
+		);
+		
+		
+		echo "<div class=\"control-group\">";
+		echo form_label('Contact Surname', 'contactSurname', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_input($contactSurname);
+		echo '</div></div>';
+		
+		$password = array(
+			'name'	=> 'password',
+			'id'	=> 'password',
+			'value' => $password
+		);
+		
+		echo "<div class=\"control-group\">";
+		echo form_label('Password', 'password', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_password($password);
+		echo '</div></div>';
+
+		$cpassword = array(
+			'name'	=> 'cpassword',
+			'id'	=> 'cpassword',
+			'value' => $cpassword
+		);
+		
+		
+		echo "<div class=\"control-group\">";
+		echo form_label('Confirm Password', 'cpassword', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_password($cpassword);
+		echo '</div></div>';
+		
+		$email = array(
+			'name'	=> 'email',
+			'id'	=> 'email',
+			'value' => $email
+		);
+
+			
+		echo "<div class=\"control-group\">";
+		echo form_label('E-mail', 'email', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_input($email);
+		echo '</div></div>';
+		
+		echo "<div class=\"control-group\">";
+		echo form_label('Logo', 'logo', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo '<input type="file" name="userfile" size="20" />';
+		echo '</div></div>';
+		
+		$descriptionField = array(
+		'name'	=> 'description',
+		'id'	=> 'description',
+		'class' => 'input-xxlarge',
+		'value' => $description
+		);
+	
+		echo "<div class=\"control-group\">";
+		echo form_label('Description', 'description', $labelAttributes);
+		echo "<div class=\"controls\">";
+		echo form_textarea($descriptionField);
+		echo '</div>';
+		echo '</div>';
+			
+		
+		echo "<div class=\"control-group\">";
+		echo "
+		<table class=\"table\">
+		<thead>
+			<tr>
+				<th></th>
+				<th>First Name</th>
+				<th>Surname</th>
+				<th>Shirt Number</th>
+			</tr> 
+		</thead>
+		<tbody>";
+		for ($i = 0; $i < 10; $i++)
+		{
+			$firstNameProp = array(
+				'name'	=> 'firstName[]',
+				'id'	=> 'firstName[]',
+				'value' => isset($firstName[$i]) ? $firstName[$i] : ""
+			);
+			
+			$surnameProp = array(
+				'name'	=> 'surname[]',
+				'id'	=> 'surname[]',
+				'value' => isset($surname[$i]) ? $surname[$i] : ""
+			);
+			
+			$numProp = array(
+				'name'	=> 'num[]',
+				'id'	=> 'num[]',
+				'value' => isset($num[$i]) ? $num[$i] : ""
+			);
+			echo "
+				<tr>
+				<td> Player " . ($i + 1) . " </td>
+				<td>" . form_input($firstNameProp) . " </td>
+				<td>" . form_input($surnameProp) . " </td>
+				<td>" . form_input($numProp) . " </td>
+				</tr>
+			";
+		}
+		echo "</tbody>
+		</table>";
+		
+		echo '</div>';
+		
+		echo "<div class=\"control-group\">";
+		echo "<div class=\"controls\">";
+		
+		echo "<input type=\"submit\" name=\"\" value=\"Submit\" class=\"upload\" submit />";
+
+		echo '</div>';
+		echo '</div>';
+		
+		echo form_close();
+	}
