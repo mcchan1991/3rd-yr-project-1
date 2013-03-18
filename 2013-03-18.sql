328ce84a1b775ce50aa61a71b65583e0599cd49d
Jacob Hansen <azarat@mail.tele.dk>
2013-03-18 02:10:56 +0000

    Edited the SQL a bit.

--- 2013-03-18.sql
+++ 2013-03-18.sql
@@ -7,3 +7,5 @@ WHERE players.nwaId = matchDetails.team1Id
 SELECT SUM( goal ) 
 FROM matchResults
 INNER JOIN players ON matchResults.playerId = players.playerId
+
+ALTER ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `matchResultsView` AS select (select sum(`matchResults`.`goal`) from (`matchResults` join `players` on((`matchResults`.`playerId` = `players`.`playerId`))) where (`players`.`nwaId` = `matchDetails`.`team1Id`)) AS `team1Goals`,(select sum(`matchResults`.`goal`) from (`matchResults` join `players` on((`matchResults`.`playerId` = `players`.`playerId`))) where (`players`.`nwaId` = `matchDetails`.`team2Id`)) AS `team2Goals`,`matchDetails`.`matchId` AS `matchId`,matchDetails.team1Id, matchDetails.team2Id from ((`matchDetails` join `teams` `team1`) join `teams` `team2` on(((`matchDetails`.`team1Id` = `team1`.`nwaId`) and (`matchDetails`.`team2Id` = `team2`.`nwaId`))))
