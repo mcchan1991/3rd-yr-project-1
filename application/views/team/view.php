b83a5ae26215a6defe153682ea4d89c9b39e293a
Doix <johnnydval@gmail.com>
2013-03-18 20:46:09 +0000

    place holder design for team view

--- application/views/team/view.php
+++ application/views/team/view.php
@@ -1,2 +1,39 @@
-<h3><?php echo $teamName; ?></h3>
-<img src="<?php echo base_url() . "/uploads/" . $image;?>" class="img-rounded">
+<div class="row-fluid">
+	
+	<div class="span2">
+		<img src="<?php echo base_url() . "/uploads/" . $image;?>" class="img-rounded">
+	</div>
+	
+	<div class="span10">
+		<h3><?php echo $teamName; ?></h3>
+	</div>
+	
+</div>
+
+<div class="row-fluid">
+<table class="table table-striped">
+  <caption><h4>Players</h4></caption>
+  <thead>
+    <tr>
+      <th>First Name</th>
+      <th>Surname</th>
+	  <th>Shirt Number</th>
+	  <th>Goals</th>
+	  <th>Assists</th>
+	  <th>Red Cards</th>
+	  <th>Yellow Cards</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>...</td>
+      <td>...</td>
+	  <td>...</td>
+	  <td>...</td>
+	  <td>...</td>
+	  <td>...</td>
+	  <td>...</td>
+    </tr>
+  </tbody>
+</table>
+</div>
