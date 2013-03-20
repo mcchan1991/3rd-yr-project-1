a78cb92643275726fc6145b5e1da1b470dcec65e
Doix <johnnydval@gmail.com>
2013-03-20 21:03:08 +0000

    fixed routing of team only working with numbers

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,7 +44,7 @@ $route['admin'] = "admin/welcome";
 $route['admin/login'] = "admin/login";
 $route['admin/logout'] = "admin/welcome/logout";
 $route['admin/derp'] = "admin/staff";
-$route['team/(:num)'] = "teamview/view/$1";
+$route['team/(:any)'] = "teamview/view/$1";
 
 
 /* End of file routes.php */
